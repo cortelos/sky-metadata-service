ad2312e68572641c95915a90bb92f83fe5877c4b
cortelos <acmacedo@gmail.com>
2021-05-07 13:29:22 +0100

    Adding price

--- src/main/java/com/sky/metadata/service/entity/Movie.java
+++ src/main/java/com/sky/metadata/service/entity/Movie.java
@@ -11,8 +11,17 @@ public class Movie {
 
     private @Id @GeneratedValue Long id;
     private String name;
+    private BigDecimal price;
 
     public Movie() {
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
     }
 
     public Movie(String name) {
